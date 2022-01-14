eb238282384972b5a983bca50b4990934ab3a245
BrianLevin123 <93602751+BrianLevin123@users.noreply.github.com>
2022-01-14 13:45:29 -0500

    created spotify authentification credentials and created access token to get the user

--- spotify/src/App.js
+++ spotify/src/App.js
@@ -1,12 +1,28 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import './App.css';
-import Login from "./Login"
+import Login from "./Login";
+import {getTokenFromUrl} from './spotify'
 
 function App() {
+  const [token, setToken] = useState(null);
+  useEffect(()=> {
+const hash = getTokenFromUrl();
+window.location.hash= "";
+const _token = hash.access_token;
+
+ if (_token) {
+   setToken(_token)
+ }
+console.log('I have a token', token)
+  }, []);
   return (
     <div className="app">
-     
-      <Login />
+     {
+       token ?  (
+         <h1>I am logged in</h1>
+       ) :  (<Login />)
+     }
+      
     </div>
   );
 }
