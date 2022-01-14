20e8fde62a697e2cf6717a14e3d736d3917c7fd0
BrianLevin123 <93602751+BrianLevin123@users.noreply.github.com>
2022-01-14 14:18:34 -0500

    created the token  and web api to get user account information

--- spotify/src/App.js
+++ spotify/src/App.js
@@ -2,6 +2,9 @@ import React, {useEffect, useState} from "react";
 import './App.css';
 import Login from "./Login";
 import {getTokenFromUrl} from './spotify'
+import SpotifyWebApi from "spotify-web-api-js";
+
+const spotify = new SpotifyWebApi();
 
 function App() {
   const [token, setToken] = useState(null);
@@ -12,6 +15,10 @@ const _token = hash.access_token;
 
  if (_token) {
    setToken(_token)
+   spotify.setAccessToken(_token);
+   spotify.getMe().then(user => {
+     console.log( ':)' , user)
+   })
  }
 console.log('I have a token', token)
   }, []);
