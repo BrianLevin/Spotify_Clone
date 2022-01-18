3f0e773b0a04d4b9edd594367703c686211aa1ce
BrianLevin123 <93602751+BrianLevin123@users.noreply.github.com>
2022-01-18 17:00:01 -0500

    made a new file

--- spotify/src/Player.js
+++ spotify/src/Player.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import './Player.css';
+import React from "react";
+import "./Player.css";
+import Sidebar  from "./Sidebar"
 
-function Player({spotify}) {
-    return (
-        <div className= "player">
-           {/* Side Bar*/}
-           {/* Body*/}
-           {/*  Footer*/}
-        </div>
-    )
+function Player({ spotify }) {
+  return (
+    <div className="player">
+      <div vlass="player__body">
+        <Sidebar/>
+        {/* Body*/}
+      </div>
+
+      {/*  Footer*/}
+    </div>
+  );
 }
 
-export default Player
+export default Player;
