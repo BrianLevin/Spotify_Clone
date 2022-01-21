3af111a6456bb7b7e325b843eef1e1b6c52ea45f
BrianLevin123 <93602751+BrianLevin123@users.noreply.github.com>
2022-01-21 15:19:52 -0500

    added animations to the side bar menue

--- spotify/src/Sidebar.js
+++ spotify/src/Sidebar.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import "./Sidebar.css";
+import SidebarOption from "./SidebarOption";
 
 function Sidebar() {
     return (
         <div className= "sidebar">
         <img 
         className= "sidebar__logo"
-        src= "https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" />
+        src= "https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" 
+        
+        />
+        <SidebarOption title= "Home" />
+        <SidebarOption  title= "Search" />
+        <SidebarOption  title= " Your Library" />
+
         </div>
     )
 }
---
+++ spotify/src/SidebarOption.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./SidebarOption.css"
+
+function SidebarOption({title,icon}) {
+    return (
+        <div className="sidebarOption">
+            <p>{title}</p>
+        </div>
+    )
+}
+
+export default SidebarOption
