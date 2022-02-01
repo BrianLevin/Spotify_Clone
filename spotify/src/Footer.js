12c1985141f70f9f4d8e36bba83cb2e9f60c00f6
BrianLevin123 <93602751+BrianLevin123@users.noreply.github.com>
2022-02-01 17:00:08 -0500

    added footer changes

--- spotify/src/Footer.js
+++ spotify/src/Footer.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React from "react";
 import "./Footer.css";
 
 function Footer() {
-    return (
-        <div className="footer">
-            <h1> Im the footer</h1>
-        </div>
-    )
+  return (
+    <div className="footer">
+      <div className="footer__left">
+        <p>Album and some detaiuls</p>
+      </div>
+      <div className="footer__center">
+        <p>Player Controls</p>
+      </div>
+      <div className="footer__right">
+        <p> Volume controls</p>
+      </div>
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
